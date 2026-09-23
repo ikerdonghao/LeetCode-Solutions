614452e0584dd7106faba464a174798608584155
ikerdonghao <31871275+ikerdonghao@users.noreply.github.com>
2026-09-22 22:42:23 -0400

    Time: 550 ms (80.98%), Space: 0B (100%) - LeetHub

--- 3601-find-drivers-with-improved-fuel-efficiency/3601-find-drivers-with-improved-fuel-efficiency.sql
+++ 3601-find-drivers-with-improved-fuel-efficiency/3601-find-drivers-with-improved-fuel-efficiency.sql
@@ -1,30 +1,22 @@
 # Write your MySQL query statement below
-WITH first_half_agg AS
-(SELECT
-    driver_id,
-    AVG(distance_km/fuel_consumed) as first_half_avg
+WITH summary as
+(
+    SELECT
+        driver_id,
+        AVG(CASE WHEN MONTH(trip_date) <= 6 then distance_km / fuel_consumed end) as first_half_avg,
+        AVG(CASE WHEN MONTH(trip_date) > 6 then distance_km / fuel_consumed end) as second_half_avg
     FROM trips
-    WHERE month(trip_date) <= 6
-    GROUP BY 1
+    GROUP BY driver_id
 )
 
-, second_half_agg AS
-(SELECT
-    driver_id,
-    AVG(distance_km/fuel_consumed) as second_half_avg
-    FROM trips
-    WHERE month(trip_date) > 6
-    GROUP BY 1
-)
-
-SELECT
-    fa.driver_id,
+SELECT 
+    s.driver_id,
     driver_name,
     round(first_half_avg,2) as first_half_avg,
     round(second_half_avg,2) as second_half_avg,
-    round(second_half_avg-first_half_avg,2) as efficiency_improvement
-FROM first_half_agg fa
-    JOIN second_half_agg sa ON fa.driver_id = sa.driver_id
-    LEFT JOIN drivers ON fa.driver_id = drivers.driver_id
-WHERE second_half_avg > first_half_avg
-ORDER BY efficiency_improvement DESC,driver_name
+    round(second_half_avg - first_half_avg,2) as efficiency_improvement 
+FROM summary s
+    LEFT JOIN drivers d ON s.driver_id = d.driver_id
+WHERE first_half_avg is not null and second_half_avg is not null
+    and second_half_avg > first_half_avg
+ORDER BY efficiency_improvement DESC, driver_name
